92ef011a873f6ad54b3103e89dd8a8b7300aab8b
Angiea18 <angie.a.zapata@gmail.com>
2024-11-11 14:14:12 -0500

    Option

--- src/main/java/com/api/estramipyme/respositories/OptionRepository.java
+++ src/main/java/com/api/estramipyme/respositories/OptionRepository.java
@@ -3,5 +3,8 @@ package com.api.estramipyme.respositories;
 import com.api.estramipyme.models.Option;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OptionRepository extends JpaRepository<Option, Long> {
+    List<Option> findByQuestionId(Long questionId);
 }
