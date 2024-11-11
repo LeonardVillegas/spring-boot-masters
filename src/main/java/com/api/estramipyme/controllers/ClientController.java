44b64c9771ae93c757dacccf522cf1cf032cd17e
Leonard Villegas <leonard.villegas@sourcemeridian.com>
2024-11-11 11:24:40 -0500

    Add get all clients, retrieve one cliente, create one client

---
+++ src/main/java/com/api/estramipyme/controllers/ClientController.java
@@ -0,0 +1,41 @@
+package com.api.estramipyme.controllers;
+
+import com.api.estramipyme.models.Client;
+import com.api.estramipyme.services.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/clients")
+public class ClientController {
+    private final ClientService clientService;
+
+    @Autowired
+    public ClientController(ClientService clientService) {
+        this.clientService = clientService;
+    }
+    // trae todos los clientes
+    @GetMapping
+    public List<Client> getAllClients() {
+        return clientService.getAllClients();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Client> getClientById(@PathVariable Long id) {
+        Optional<Client> client = clientService.getClientById(id);
+        return client.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<Client> createClient(@RequestBody Client client) {
+        Client savedClient = clientService.saveClient(client);
+        return new ResponseEntity<>(savedClient, HttpStatus.CREATED);
+    }
+
+}
