83840503e80a694e4e17dcae95f5d9b75bd55231
qtenho <edwincuteno12@gmail.com>
2024-11-12 13:59:33 -0500

    create crud response model

--- src/main/java/com/api/estramipyme/respositories/ResponseRepository.java
+++ src/main/java/com/api/estramipyme/respositories/ResponseRepository.java
@@ -1,4 +1,7 @@
 package com.api.estramipyme.respositories;
 
-public interface ResponseRepository {
+import com.api.estramipyme.models.Response;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ResponseRepository extends JpaRepository<Response, Long> {
 }
--- src/main/java/com/api/estramipyme/services/ResponseService.java
+++ src/main/java/com/api/estramipyme/services/ResponseService.java
@@ -1,4 +1,45 @@
 package com.api.estramipyme.services;
 
+import com.api.estramipyme.models.Response;
+import com.api.estramipyme.respositories.ResponseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class ResponseService {
+
+    private final ResponseRepository responseRepository;
+
+    @Autowired
+    public ResponseService(ResponseRepository responseRepository) {
+
+        this.responseRepository = responseRepository;
+    }
+
+    // Obtener todas las respuestas
+    public List<Response> getAllResponse() {
+
+        return responseRepository.findAll();
+    }
+
+    // Obtener una respuesta por su ID
+    public Optional<Response> getResponseById(Long id) {
+
+        return responseRepository.findById(id);
+    }
+
+    //Crear una nueva respuesta
+    public Response saveResponse(Response response) {
+        return responseRepository.save(response);
+    }
+
+    // Eliminar una respuesta por su ID
+    public void deleteResponseById(Long id) {
+
+       responseRepository.deleteById(id);
+    }
+
 }
