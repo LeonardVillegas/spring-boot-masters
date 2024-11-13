b6a75d6bb6feb7f211bb6a4958ae4d7771901318
Leonard Villegas <leonard.villegas@sourcemeridian.com>
2024-11-13 09:57:22 -0500

    Agregar documentación de la api

---
+++ src/main/java/com/api/estramipyme/config/OpenAPIConfig.java
@@ -0,0 +1,16 @@
+package com.api.estramipyme.config;
+
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public") // Nombre del grupo de API
+                .pathsToMatch("/api/**") // Rutas específicas a documentar
+                .build();
+    }
+}
