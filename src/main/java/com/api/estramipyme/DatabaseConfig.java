c917e25d8d8e294f0b779bba308391bd37007b2f
Leonard Villegas <leonard.villegas@sourcemeridian.com>
2024-11-12 19:30:09 -0500

    Solve conflict

--- src/main/java/com/api/estramipyme/DatabaseConfig.java
+++ src/main/java/com/api/estramipyme/DatabaseConfig.java
@@ -1,4 +1,7 @@
 package com.api.estramipyme;
+import com.api.estramipyme.models.Question;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.json.JSONObject;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
